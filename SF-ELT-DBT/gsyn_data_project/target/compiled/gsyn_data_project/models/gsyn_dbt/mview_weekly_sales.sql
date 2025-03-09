0693270d00f8936fd29fe588c506bffa64682777
paparao-7 <85115683+paparao-7@users.noreply.github.com>
2025-03-09 20:33:53 +0530

    Add files via upload

---
+++ SF-ELT-DBT/gsyn_data_project/target/compiled/gsyn_data_project/models/gsyn_dbt/mview_weekly_sales.sql
@@ -0,0 +1,22 @@
+
+
+WITH sales AS (
+    SELECT 
+        st.pos_site_id,
+        st.sku_id,
+        fc.fsclwk_id,
+        st.price_substate_id,
+        st.type,
+        SUM(st.sales_units) AS total_sales_units,
+        SUM(st.sales_dollars) AS total_sales_dollars,
+        SUM(st.discount_dollars) AS total_discount_dollars
+    FROM gsyn_db.gsyn_fct.sales_transactions st
+    JOIN gsyn_db.gsyn_dim.fiscal_calendar fc 
+        ON st.fscldt_id = fc.fscldt_id
+
+    
+
+    GROUP BY st.pos_site_id, st.sku_id, fc.fsclwk_id, st.price_substate_id, st.type
+)
+
+SELECT * FROM sales
